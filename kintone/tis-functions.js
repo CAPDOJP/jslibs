8cc34ad64cfacf0107fbe941638b046946aa4cf1
TIS <info@tis2010.jp>
2016-08-22 06:10:45 +0000

    commit

--- kintone/tis-functions.js
+++ kintone/tis-functions.js
@@ -91,16 +91,17 @@ jQuery.fn.fields=function(fieldcode){
 			});
 		}
 	});
-	if (cybozu.data.page.FORM_DATA.schema.subTable.length!=0)
-		$.each(cybozu.data.page.FORM_DATA.schema.subTable.fieldList,function(key,values){
-			if (values.var==fieldcode)
-			{
-				$.each(target.find('[id*='+key+'],[name*='+key+']'),function(index){
-					if ($(this).prop('tagName').toLowerCase()!='undefined')
-						if ($.inArray($(this),fields)==-1) fields.push($(this));
-				});
-			}
-		});
+	if ('subTable' in cybozu.data.page.FORM_DATA.schema)
+		if ('fieldList' in cybozu.data.page.FORM_DATA.schema.subTable)
+			$.each(cybozu.data.page.FORM_DATA.schema.subTable.fieldList,function(key,values){
+				if (values.var==fieldcode)
+				{
+					$.each(target.find('[id*='+key+'],[name*='+key+']'),function(index){
+						if ($(this).prop('tagName').toLowerCase()!='undefined')
+							if ($.inArray($(this),fields)==-1) fields.push($(this));
+					});
+				}
+			});
 	return fields;
 }
 /*
