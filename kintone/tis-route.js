d78010b62d579ffe1d2d369859518b0ed4bae8ce
TIS <info@tis2010.jp>
2016-08-24 12:38:09 +0000

    commit

--- kintone/tis-route.js
+++ kintone/tis-route.js
@@ -23,7 +23,7 @@ var RouteMap=function(apiikey){
     };
     /* append elements */
 	this.container=div.clone(true).css({
-		'bottom':'-50%',
+		'bottom':'-10%',
 		'height':'100%',
 		'left':'0px',
 		'position':'fixed',
