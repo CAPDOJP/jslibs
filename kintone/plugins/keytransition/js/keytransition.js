432ac21aa00cb06ac85965516e7404079fc11d78
TIS <info@tis2010.jp>
2017-01-20 06:40:12 +0000

    commit

--- kintone/plugins/keytransition/js/keytransition.js
+++ kintone/plugins/keytransition/js/keytransition.js
@@ -15,23 +15,8 @@ jQuery.noConflict();
 	 valiable
 	---------------------------------------------------------------*/
 	var vars={
-		excludes:[
-			'CALC',
-			'CATEGORY',
-			'CREATED_TIME',
-			'CREATOR',
-			'FILE',
-			'GROUP_SELECT',
-			'MODIFIER',
-			'ORGANIZATION_SELECT',
-			'RECORD_NUMBER',
-			'RICH_TEXT',
-			'STATUS',
-			'STATUS_ASSIGNEE',
-			'SUBTABLE',
-			'UPDATED_TIME',
-			'USER_SELECT'
-		]
+		controls:'a:visible:not(:disabled),button:visible:not(:disabled),input[type=button]:visible:not(:disabled),input[type=text]:visible:not(:disabled),select:visible:not(:disabled),textarea:visible:not(:disabled)',
+		dialogs:null
 	};
 	var events={
 		show:[
@@ -45,25 +30,39 @@ jQuery.noConflict();
 	kintone.events.on(events.show,function(event){
 		var config=kintone.plugin.app.getConfig(PLUGIN_ID);
 		if (!config) return false;
+		/* initialize valiable */
+		vars.dialogs=$('body').children('div[class*=dialog]');
+	    /* view events */
+	    vars.dialogs.on('inview',function(){$(this).find(vars.controls).first().focus();});
 	    /* key events */
-		$(document).on('keydown','input[type=text],select',function(e){
+		$(document).on('keydown','a,button,input[type=button],input[type=text],select',function(e){
 			var code=e.keyCode||e.which;
-			if (code==13)
+			var direction='';
+			if (code==13 && ($(this).prop('tagName').toLowerCase()!='button' && $(this).prop('tagName').toLowerCase()!='a' && $(this).prop('tagName').toLowerCase()!='input[type=button]')) direction=(e.shiftKey)?'prev':'next';
+			if (code==38 && $(this).prop('tagName').toLowerCase()!='select') direction='prev';
+			if (code==40 && $(this).prop('tagName').toLowerCase()!='select') direction='next';
+			if (direction.length!=0)
 			{
-				var targets=$(this).closest('form').find('button:visible:not(:disabled),,input[type=button]:visible,input[type=text]:visible:not(:disabled),select:visible:not(:disabled),textarea:visible:not(:disabled)');
+				var range=$(this).parents('div').last();
+				var targets=null;
+				if (!range.is('div[class*=dialog]')) range=$('body');
+				targets=range.find(vars.controls);
 				var total=targets.length;
 				var index=targets.index(this);
-				if (e.shiftKey)
+				switch (direction)
 				{
-					if (index==0) index=total;
-					index--;
-				}
-				else
-				{
-					index++;
-					if (index==total) index=0;
+					case 'prev':
+						if (index==0) index=total;
+						index--;
+						break;
+					case 'next':
+						index++;
+						if (index==total) index=0;
+						break;
 				}
 				targets.eq(index).focus();
+				e.preventDefault();
+				e.stopPropagation();
 				return false;
 			}
 		});
