b1a30579108dfe4766dfdd6ee315668fa6082297
TIS <info@tis2010.jp>
2017-04-05 10:46:04 +0000

    commit

--- plugins/jquery.formAction.js
+++ plugins/jquery.formAction.js
@@ -1291,7 +1291,7 @@ var oldshow = $.fn.show;
 jQuery.fn.show = function()
 {
     var ret=oldshow.apply(this, arguments);
-    if (!this.parent(':hidden').size()) this.css({'visibility':'visible'});
+    if (this.parent(':hidden').length==0) this.css({'visibility':'visible'});
     return ret;
 };
 /*
