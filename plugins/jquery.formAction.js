bd96005ed34293024fe418ac56c311a52bbe7255
TIS <info@tis2010.jp>
2017-04-05 23:51:05 +0000

    commit

--- plugins/jquery.formAction.js
+++ plugins/jquery.formAction.js
@@ -1284,25 +1284,6 @@ String.prototype.bytelength = function(){
 }
 /*
 *--------------------------------------------------------------------
-* jQuery関数拡張
-* -------------------------------------------------------------------
-*/
-var oldshow = $.fn.show;
-jQuery.fn.show = function()
-{
-    var ret=oldshow.apply(this, arguments);
-	if ($(this).prop('tagName').toLowerCase()!='iframe')
-	{
-		var hidden=false;
-		$.each($(this).parents(),function(){
-			if ($(this).css('visibility')=='hidden') hidden=true;
-		});
-	    if (!hidden) $(this).css({'visibility':'visible'});
-	}
-    return ret;
-};
-/*
-*--------------------------------------------------------------------
 * データ参照ウインドウ
 *--------------------------------------------------------------------
 * parameters
