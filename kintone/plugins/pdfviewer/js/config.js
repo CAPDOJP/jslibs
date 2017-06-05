0db6afb34b7085fb8fb1114ab30f129c5517a929
TIS <info@tis2010.jp>
2017-06-05 05:25:43 +0000

    commit

---
+++ kintone/plugins/pdfviewer/js/config.js
@@ -0,0 +1,22 @@
+/*
+*--------------------------------------------------------------------
+* jQuery-Plugin "pdfviewer -config.js-"
+* Version: 1.0
+* Copyright (c) 2017 TIS
+*
+* Released under the MIT License.
+* http://tis2010.jp/license.txt
+* -------------------------------------------------------------------
+*/
+jQuery.noConflict();
+(function($,PLUGIN_ID){
+	"use strict";
+	/*---------------------------------------------------------------
+	 button events
+	---------------------------------------------------------------*/
+	$('button#submit').on('click',function(e){
+        var config=[];
+		/* save config */
+		kintone.plugin.app.setConfig(config);
+	});
+})(jQuery,kintone.$PLUGIN_ID);
