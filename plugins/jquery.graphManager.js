bdb3e0296b20c585e8dfefc6bdcd74afb310ce89
TIS <info@tis2010.jp>
2017-09-25 10:55:45 +0000

    commit

--- plugins/jquery.graphManager.js
+++ plugins/jquery.graphManager.js
@@ -231,8 +231,7 @@ graphManager.prototype={
 						}
 						left+=caption.width;
 					});
-					my.context.imageSmoothingEnabled=false;
-					my.context.lineWidth=2;
+					my.context.lineWidth=1.5;
 					my.context.strokeStyle=my.markers[index].color;
 					if (my.markers[index].dot)
 					{
