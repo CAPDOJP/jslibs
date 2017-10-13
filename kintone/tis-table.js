e6846c537da9687ce0b018dbbdfe8e200b933490
TIS <info@tis2010.jp>
2017-10-13 03:18:44 +0000

    commit

--- kintone/tis-table.js
+++ kintone/tis-table.js
@@ -73,9 +73,8 @@ var Table=function(options){
 		var row=$(this).parent();
 		var rowindex=contents.find('tr').index(row);
 		var cellindex=row.find('td').index($(this));
-		console.log(e.pageX);
-		scrollPos.x=my.container.offset().left;
-		scrollPos.y=my.container.offset().top;
+		scrollPos.x=e.pageX;
+		scrollPos.y=e.pageY;
 		if (options.mergeexclude.indexOf(my.cellindex(row,cellindex))==-1)
 		{
 			/* merge start */
@@ -177,12 +176,11 @@ var Table=function(options){
 		}
 		else
 		{
-			console.log(e.pageX);
 			if (
-				scrollPos.x>my.container.offset().left-5 &&
-				scrollPos.x<my.container.offset().left+5 &&
-				scrollPos.y>my.container.offset().top-5 &&
-				scrollPos.y<my.container.offset().top+5 &&
+				scrollPos.x>e.pageX-5 &&
+				scrollPos.x<e.pageX+5 &&
+				scrollPos.y>e.pageY-5 &&
+				scrollPos.y<e.pageY+5 &&
 				options.unmergetrigger!=null
 			)
 				options.unmergetrigger(
