eacd0802887faa96e939adcb6a3c50a9a74ad0f0
TIS <info@tis2010.jp>
2017-10-20 14:21:19 +0000

    commit

--- kintone/tis-calendar.js
+++ kintone/tis-calendar.js
@@ -41,10 +41,20 @@ var Calendar=function(options){
 	this.params=options;
 	/* valiable */
 	var my=this;
+	var calendars={
+		height:0,
+		width:0,
+		margin:{
+			x:5,
+			y:10
+		}
+	};
 	var cells=30;
 	var rows=8;
 	var div=$('<div>').css({
 		'box-sizing':'border-box',
+		'margin':'0px',
+		'padding':'0px',
 		'position':'relative'
 	});
 	var button=$('<button>').css({
@@ -57,13 +67,18 @@ var Calendar=function(options){
 		'cursor':'pointer',
 		'font-size':'13px',
 		'height':cells.toString()+'px',
+		'line-height':cells.toString()+'px',
 		'margin':'0px',
 	    'outline':'none',
 	    'padding':'0px',
 		'width':cells.toString()+'px'
 	});
 	var table=$('<table>');
+	var week=['日','月','火','水','木','金','土'];
+	calendars.height=cells*rows+calendars.margin.y;
+	calendars.width=cells*week.length+(calendars.margin.x*2);
 	/* append elements */
+	this.calendars=[];
 	this.cover=div.clone(true).css({
 		'background-color':'rgba(0,0,0,0.5)',
 		'display':'none',
@@ -79,17 +94,17 @@ var Calendar=function(options){
 		'bottom':'0',
 		'border-radius':'5px',
 		'box-shadow':'0px 0px 3px rgba(0,0,0,0.35)',
-		'height':((250+(rows-1))*Math.ceil(options.span/3)+((cells*2)+5)).toString()+'px',
+		'height':((calendars.height+(rows-1))*Math.ceil(options.span/3)+(cells*((options.multi)?3:2))).toString()+'px',
 		'left':'0',
 		'margin':'auto',
 		'max-height':'100%',
 		'max-width':'100%',
-		'padding':cells.toString()+'px 5px 5px 5px',
+		'padding':(cells*2).toString()+'px 0px 0px '+((options.multi)?cells:0).toString()+'px',
 		'position':'absolute',
 		'right':'0',
 		'text-align':'center',
 		'top':'0',
-		'width':(220*((options.span>3)?3:options.span)+10).toString()+'px'
+		'width':(calendars.width*((options.span>3)?3:options.span)+10).toString()+'px'
 	})
 	.append(
 		button.clone(true).css({
@@ -100,57 +115,87 @@ var Calendar=function(options){
 			'z-index':options.span+1
 		})
 		.on('click',function(){my.cover.hide();})
-	);
-	this.contents=[];
-	this.feedblock=div.clone(true).css({
-		'height':cells.toString()+'px',
-		'width':'100%'
-	})
-	.append(
-		button.clone(true).css({
-			'background-image':'url("https://rawgit.com/TIS2010/jslibs/master/kintone/plugins/images/prev.png")',
-			'position':'absolute',
-			'left':'0px',
-			'top':'0px'
-		})
-		.on('click',function(){
-			/* calc months */
-			my.displaymonth=my.displaymonth.calc('-'+options.span.toString()+' month').calc('first-of-month');
-			/* display calendar */
-			my.show();
-		})
 	)
 	.append(
-		button.clone(true).css({
-			'background-image':'url("https://rawgit.com/TIS2010/jslibs/master/kintone/plugins/images/next.png")',
+		div.clone(true).css({
+			'height':cells.toString()+'px',
+			'left':'0px',
 			'position':'absolute',
-			'right':'0px',
-			'top':'0px'
+			'top':cells.toString()+'px',
+			'z-index':options.span+2,
+			'width':'100%'
 		})
-		.on('click',function(){
-			/* calc months */
-			my.displaymonth=my.displaymonth.calc(options.span.toString()+' month').calc('first-of-month');
-			/* display calendar */
-			my.show();
-		})
+		.append(
+			button.clone(true).css({
+				'background-image':'url("https://rawgit.com/TIS2010/jslibs/master/kintone/plugins/images/prev.png")',
+				'position':'absolute',
+				'left':'0px',
+				'top':'0px'
+			})
+			.on('click',function(){
+				/* calc months */
+				my.displaymonth=my.displaymonth.calc('-'+options.span.toString()+' month').calc('first-of-month');
+				/* display calendar */
+				my.show();
+			})
+		)
+		.append(
+			button.clone(true).css({
+				'background-image':'url("https://rawgit.com/TIS2010/jslibs/master/kintone/plugins/images/next.png")',
+				'position':'absolute',
+				'right':'0px',
+				'top':'0px'
+			})
+			.on('click',function(){
+				/* calc months */
+				my.displaymonth=my.displaymonth.calc(options.span.toString()+' month').calc('first-of-month');
+				/* display calendar */
+				my.show();
+			})
+		)
+	);
+	this.contents=table.clone(true).css({
+		'overflow-x':'hidden',
+		'overflow-y':'auto',
+		'padding':'0px 5px',
+		'width':'100%'
+	});
+	this.buttonblock=div.clone(true).css({
+		'bottom':'0px',
+		'left':'0px',
+		'padding':'0px',
+		'position':'absolute',
+		'text-align':'center',
+		'width':'100%',
+		'z-index':options.span+3
+	})
+	.append(
+		button.clone(true)
+		.attr('id','ok')
+		.text('OK')
+	)
+	.append(
+		button.clone(true)
+		.attr('id','cancel')
+		.text('キャンセル')
 	);
 	/* create calendar */
 	for (var i=0;i<options.span;i++)
 	{
 		var calendar=table.clone(true).css({'box-sizing':'border-box'});
-		this.contents.push(
+		this.calendars.push(
 			div.clone(true).css({
 				'display':'inline-block',
-				'height':'240px',
-				'margin':'0px 5px 10px 5px',
-				'width':'210px'
+				'height':(cells*rows).toString()+'px',
+				'margin':'0px '+calendars.margin.x+'px '+calendars.margin.y+'px '+calendars.margin.x+'px',
+				'width':(cells*week.length).toString()+'px'
 			})
 			.append(calendar)
 		);
 		/* create cells */
-		for (var i2=0;i2<7*rows;i2++)
+		for (var i2=0;i2<week.length*rows;i2++)
 		{
-			if (i2%7==0) calendar.append($('<tr>'));
+			if (i2%week.length==0) calendar.append($('<tr>'));
 			calendar.find('tr').last()
 			.append(
 				$('<td>').css({
@@ -176,17 +221,16 @@ var Calendar=function(options){
 			);
 		}
 		/* create header */
-		var week=['日','月','火','水','木','金','土'];
 		calendar.find('tr').eq(0).find('td').css({'border':'none','cursor':'pointer'});
 		calendar.find('tr').eq(0).find('td').each(function(index){if (index>0) $(this).remove();});
-		calendar.find('tr').eq(0).find('td').eq(0).attr('colspan',7).css('cursor','default');
+		calendar.find('tr').eq(0).find('td').eq(0).attr('colspan',week.length).css('cursor','default');
 		calendar.find('tr').eq(1).find('td').each(function(index){$(this).text(week[index]);});
 		calendar.find('tr:gt(0)').find('td').eq(0).css({'background-color':options.sunday.back,'color':options.sunday.fore});
 		calendar.find('tr:gt(0)').find('td').eq(6).css({'background-color':options.saturday.back,'color':options.saturday.fore});
 	}
 	/* append elements */
-	this.container.append(this.feedblock);
-	$.each(this.contents,function(index){my.container.append(my.contents[index]);});
+	$.each(this.calendars,function(index){my.contents.append(my.calendars[index]);});
+	this.container.append(this.contents);
 	this.cover.append(this.container);
 	options.container.append(this.cover);
 };
@@ -213,7 +257,7 @@ Calendar.prototype={
 		for (var i=0;i<params.span;i++)
 		{
 			activedate=this.activedate;
-			calendar=this.contents[i].find('table');
+			calendar=this.calendars[i].find('table');
 			displaymonth=this.displaymonth.calc(i.toString()+' month');
 			/* initialize header title */
 			calendar.find('tr').first().find('td').eq(0).text(displaymonth.format('Y-m'));
