f14ac53ae9cf393ddb54a5b0d33006f452e9c838
TIS <info@tis2010.jp>
2017-12-11 01:00:23 +0000

    commit

--- kintone/plugins/userlinked/js/userlinked.js
+++ kintone/plugins/userlinked/js/userlinked.js
@@ -84,7 +84,6 @@ jQuery.noConflict();
 		functions.checklogin(function(resp){
 			vars.setting=resp;
 			/* check viewid */
-			console.log($.queries());
 			if (vars.setting.view.length!=0 && vars.setting.view!=event.viewId && Object.keys($.queries()).length==0)
 				window.location.href='https://'+$(location).attr('host')+'/k/'+kintone.app.getId()+'/?view='+vars.setting.view;
 		});
